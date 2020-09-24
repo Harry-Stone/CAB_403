2c1158051d4e59eaded3b84270e5941e2c0ba2e9
Harry (VM) <h3.stone@connect.qut.edu.au>
2020-09-24 10:12:51 +1000

    added parellel to overseer experimental

--- overseer.c
+++ overseer.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+#include <omp.h>
 
 int main(int argc, char *argv){
-
+    #pragma omp parallel num_threads(5)
+    {
+        int a =  omp_get_thread_num();
+        printf("thread is: %d\n", a);
+        for(int i = 1; i<= 10; ++i){
+            printf("I is: %d\n", i);
+            
+        }
+    }
     return 1;
 }
