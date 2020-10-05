62188c619c9ae4c9ec7a074c8cf5eab116d01964
karatedamo <karatedamo@gmail.com>
2020-10-06 09:49:19 +1000

    makefile template that I'm trying

--- overseer.c
+++ overseer.c
@@ -2,14 +2,21 @@
 #include <omp.h>
 
 int main(int argc, char *argv){
+
     #pragma omp parallel num_threads(5)
     {
-        int a =  omp_get_thread_num();
-        printf("thread is: %d\n", a);
-        for(int i = 1; i<= 10; ++i){
-            printf("I is: %d\n", i);
+        while(1){
+            if(haswork){
+
+            }
             
         }
+        // for(int i = 1; i<= 10; ++i){
+        //     printf("I is: %d\n", i);
+        //     int a =  omp_get_thread_num();
+        //     printf("thread is: %d\n", a);
+        //     sleep(1);
+        // }
     }
     return 1;
 }
