5999c431c0203affbda55548547b48953dbc0812
dtgabele1 <dtgabele@gmail.com>
2020-10-06 11:30:25 +1000

    Updated trythreadpool

--- trythreadpool.c
+++ trythreadpool.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <omp.h>
 
-#define NUM_THREADS 10
+#define NUM_THREADS 5
 
 void* work(void* arg){
-    printf("Hello world\n");
+    printf("Hello world: %d\n", * (int*) arg);
 }
 
 int main(){
     pthread_t pool[NUM_THREADS];
+
+    int arguments[NUM_THREADS];
+
+    for(int i = 0; i < NUM_THREADS;i++){
+        arguments[i] = i;
+    }
+
     for(int i = 0; i< NUM_THREADS;i++){
-        pthread_create(pool+i,NULL,work,NULL);
+        pthread_create(pool+i,NULL,work,arguments + i);
+    }
+
+    for(int i = 0; i< NUM_THREADS;i++){
+        pthread_join(*(pool+i),NULL);
     }
 }
