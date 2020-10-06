b675953f26b4ee6f94e39987dee1625a01436d60
dtgabele1 <dtgabele@gmail.com>
2020-10-06 11:52:49 +1000

    Udated trythreadpool

--- trythreadpool.c
+++ trythreadpool.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <omp.h>
 
-#define NUM_THREADS 5
+#define NUM_THREADS 10
+#define DATA_SIZE 100
+#define BLOCK (DATA_SIZE/ NUM_THREADS)
 
-void* work(void* arg){
-    printf("Hello world: %d\n", * (int*) arg);
+//int parallel_sum = 0;
+
+void* work(void* arg)
+{
+
 }
 
 int main(){
     pthread_t pool[NUM_THREADS];
+    
 
+    srand(50);
+
+    int data[DATA_SIZE];
+
+    for(int i = 0;i<DATA_SIZE;i++){
+        data[i] = rand() % 100;
+    }
     int arguments[NUM_THREADS];
+    int sum = 0;
+    for (int i  = 0; i < DATA_SIZE; i++){
+        sum += data[i];
+    }
 
-    for(int i = 0; i < NUM_THREADS;i++){
+    printf("sequential sum is %d\n",sum);
+
+    for(int i = 0; i < NUM_THREADS ;i++){
         arguments[i] = i;
     }
 
     for(int i = 0; i< NUM_THREADS;i++){
-        pthread_create(pool+i,NULL,work,arguments + i);
+        pthread_create(pool+i,NULL, work, arguments + i);
     }
 
     for(int i = 0; i< NUM_THREADS;i++){
