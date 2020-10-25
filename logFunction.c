6510d852940615e4510b27ba9aa104d07b51fdab
karatedamo <karatedamo@gmail.com>
2020-10-25 16:12:25 +1000

    I am actually superior when I use *

--- logFunction.c
+++ logFunction.c
@@ -3,13 +3,22 @@
 #include<string.h>
 
 char* getDate(){
-    char buf[150]; //number is just enough data to hold the date
+    // char buf[150]; //number is just enough data to hold the date
+    char *buf = malloc(20);
     time_t curtime;
     struct tm *loc_time;
     curtime = time (NULL);
-    loc_time = localtime (&curtime); //convert time to loacal
-    return asctime(loc_time);
+    loc_time = localtime (&curtime); //convert time to local
+    sprintf(buf,"%d-%02d-%02d %02d:%02d:%02d",loc_time->tm_year+1900,
+        loc_time->tm_mon,loc_time->tm_mday,loc_time->tm_hour,loc_time->tm_min,
+        loc_time->tm_sec);
+        
+    printf("%s\n",buf);
+    // return asctime(loc_time);
+
+    return buf;
 }
+
 
 int writeLog(char* message){
     FILE *logFile;
@@ -19,6 +28,7 @@ int writeLog(char* message){
     logFile = fopen(logFileAddress, "a");
     fprintf(logFile, "%s:%s\n", date, message);
     fclose(logFile);
+    free(date);
     return 1;
 }
 
--- splitting_test.c
+++ splitting_test.c
@@ -10,30 +10,50 @@
 #include <unistd.h>
 #include <time.h>
 
-char* split_arg(char str[])
+// char* split_arg(char str[])
+// {
+//     char *ptr = strtok(str, " ");
+//     char *out = malloc(strlen(str) * sizeof(char));
+//     if(out == NULL)
+// 		return NULL;
+//     // char *out[100];
+//     int i = 0;
+
+//     while (ptr != NULL)
+//     {
+//         out[i++] = ptr;
+//         ptr = strtok(NULL," ");
+//     }
+
+//     return out;
+// }
+// char str[] = "working perfectly because c is easy";
+//     char *final = split_arg(str);
+
+//     printf("%s\n",final);
+
+//     free(final);
+
+int main(int argc, char *argv[])
 {
-    char *ptr = strtok(str, " ");
-    char *out = malloc(strlen(str) * sizeof(char));
-    if(out == NULL)
-		return NULL;
-    // char *out[100];
-    int i = 0;
-
-    while (ptr != NULL)
+	// char str[] = "we are all very happy";
+    // strcpy(str,*argv);
+    string sstr;
+    for (int i = 0; i < argc; i++)
     {
-        out[i++] = ptr;
-        ptr = strtok(NULL," ");
+        
     }
+    
 
-    return out;
-}
+	char delim[] = " ";
+    printf("%s",argv[0]);
+	char *ptr = strtok(argv, delim);
+    // char *out[100];
+	while (ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+		ptr = strtok(NULL, delim);
+	}
 
-int main(void){
-    char str[] = "working perfectly because c is easy";
-    char *final = split_arg(str);
-
-    printf("%s\n",final);
-
-    free(final);
-
+	return 0;
 }
